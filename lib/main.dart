b75f1beffcf02eed4cc9c5ea0f70f447ce81c551
hanool <29415530+hanool@users.noreply.github.com>
2021-02-28 13:06:13 +0900

    add scrolling list view

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,7 @@ class MyApp extends StatelessWidget {
     
     return MaterialApp(
       title: 'Flutter App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome'),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
-      ),
+      home: RandomWords(),
     );
   }
 }
@@ -27,10 +20,42 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = TextStyle(fontSize: 18.0);
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return Divider();
+
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+    appBar: AppBar(
+      title: Text('Name Generator')
+    ),
+    body: _buildSuggestions(),);
   }
 }
+  
+
+  
